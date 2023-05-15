b3b38538e38495ae614b12031ac29f608a1a46c4
Tim Pilius <tpilius@gmail.com>
2023-05-15 15:08:39 -0400

    add mkdocs

---
+++ _startMkdocs.ps1
@@ -0,0 +1,12 @@
+# Check dead links
+# muffett http://localhost:8000
+
+
+
+pip install mkdocs
+pip install mkdocs-awesome-pages-plugin
+pip install mkdocs-static-i18n
+
+cd docs
+start http://127.0.0.1:8000
+python3 -m mkdocs serve
---
+++ docs/mkdocs/detailed-command-usage/Select-Apps.md
@@ -0,0 +1,4 @@
+# select-apps
+
+<div data-cli-player="../casts/select-apps.cast" data-rows=30></div>
+<br>
